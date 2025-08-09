96f87776b9d0f099a7a899e237a25beaff82753d
ljorente <ljorente@fattail.com>
2025-08-09 16:09:51 -0300

    update

---
+++ test/controller/userController.test.js
@@ -0,0 +1,22 @@
+//bibliotecas
+const request = require('supertest');
+const sinon = require('sinon'); 
+const { expect } = require('chai');
+
+//aplicação
+const app = require('../../app');
+
+describe('User Controller Tests', () => {
+    describe('POST /transfer', () => {
+        it('quando retorno dados validos o retorno é 200',async () => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: 'user1',
+                    to: 'user2',
+                    value: 100
+                });
+            expect(resposta.status).to.equal(200);
+       })
+    })
+})
